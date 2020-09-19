a4e792241a9b4f543016297a85b1ac2ae79462a7
opoink <37372895+opoink@users.noreply.github.com>
2020-09-19 16:31:19 +0800

    base controller added template ref is added as well

--- Config.php
+++ Config.php
@@ -1,8 +1,12 @@
 <?php
-return [
-   'vendor' => 'Opoink',
-   'module' => 'Bmodule',
-   'version' => '1.0.0',
-   'controllers' => [],
-];
+return array (
+  'vendor' => 'Opoink',
+  'module' => 'Bmodule',
+  'version' => '1.0.0',
+  'controllers' => 
+  array (
+    'admin_index_index_index' => '\\Opoink\\Bmodule\\Controller\\Admin\\Index\\Index\\Index',
+    'index_index_index' => '\\Opoink\\Bmodule\\Controller\\Index\\Index\\Index',
+  ),
+)
 ?>
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # bmodule
 Opoink base module/estension
+
+# Introduction
+This module is created for opoink framework to be your base module for your project. The template used on this module came from AdminLTE
+https://adminlte.io/themes/v3/ 
+https://github.com/colorlibhq/AdminLTE
