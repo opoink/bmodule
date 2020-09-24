e7e2ddda87e0618254473fee7e06be2a196a3830
Ron <leentechdev@gmail.com>
2020-09-24 12:01:28 +0800

    use copy dir instead of symlink

--- Controller/Admin/Controller.php
+++ Controller/Admin/Controller.php
@@ -4,9 +4,30 @@ namespace Opoink\Bmodule\Controller\Admin;
 class Controller extends \Of\Controller\Controller {
 
 	public function __construct(){
-		if(!is_link(ROOT.DS.'dist')){
-			symlink(ROOT.DS.'App'.DS.'Ext'.DS.'Opoink'.DS.'Bmodule'.DS.'View'.DS.'dist', ROOT.DS.'dist');
+		$this->symLinkDist();
+	}
+
+	protected function symLinkDist(){
+		$deployDir = ROOT.DS.'public'.DS.'deploy';
+
+		$generationFile = ROOT.DS.'public'.DS.'generation.php';
+		if(file_exists($generationFile)){
+			$deployDir .= include($generationFile);
 		}
+
+		// if(!is_dir($deployDir)){
+		// 	$dirMan = new \Of\File\Dirmanager();
+		// 	$dirMan->createDir($deployDir);
+		// }
+
+		// if(is_dir($deployDir)){
+			$dist = $deployDir.DS.'dist';
+			if(!is_dir($dist)){
+
+				$dirMan = new \Of\File\Dirmanager();
+				$dirMan->copyDir(ROOT.DS.'App'.DS.'Ext'.DS.'Opoink'.DS.'Bmodule'.DS.'View'.DS.'dist', $dist);
+			}
+		// }
 	}
 
 	public function run(){
