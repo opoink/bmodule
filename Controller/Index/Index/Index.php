f40e530355a7c0adeef8aabc144b83d68848e730
opoink <37372895+opoink@users.noreply.github.com>
2020-09-28 23:06:26 +0800

    nothing is important for now just test for new database module
    nothing is important for now just test for new database module

--- Controller/Index/Index/Index.php
+++ Controller/Index/Index/Index.php
@@ -9,11 +9,26 @@ class Index extends \Of\Controller\Controller {
 
 	public function __construct(
 		\Of\Http\Url $Url,
-		\Of\Std\Message $Message
+		\Of\Std\Message $Message,
+		\Of\Database\Entity $Entity
 	){
 
 		$this->_url = $Url;
 		$this->_message = $Message;
+		$entity = $Entity;
+
+		// // var_dump($this->_di->get('\Of\Database\Connection')->getConnection());
+		// $this->Connection->setConfig()->connect();
+		$entity->getConnection();
+		$entity->_select->from(['profile' => 'p'])
+		->from(['users' => 'u'])
+		->select([
+			'u.name' => 'uname', 
+			'p.email' => 'pemail', 
+			'p.age' => 'page'
+		]);
+
+		$entity->_select->getLastQuery();
 	}
 
 	public function run(){
