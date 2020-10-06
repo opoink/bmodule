17f932d755236352bf986502c780c4233f79150e
opoink <ronromeperez@yahoo.com>
2020-10-06 00:21:15 +0000

    updates during testing query builder

--- Entity/User.php
+++ Entity/User.php
@@ -4,45 +4,10 @@ namespace Opoink\Bmodule\Entity;
 class User extends \Of\Database\Entity {
 
 	public function testQry(){
-		// // var_dump($this->_di->get('\Of\Database\Connection')->getConnection());
-		// $this->Connection->setConfig()->connect();
-
 		
-		// $s = $this->getSelect()
-		// ->select(['u.email'])
-		// ->select(['up.firstname' => 'upfirstname'])
-		// ->from(['user' => 'u', 'users_profile' => 'up'])
-		// ->where('u.pass')->eq('asdasd')
-		// ->where('u.pass')->eq('asdasd')
-		// ->where('u.email')->eq(function($subquery){
-			// $subquery->_whereStatement->valPrefix = 'sub';
-			// $subquery
-			// ->from(['user' => 'u'])
-			// ->select(['u.email'])
-			// ->where('u.pass')->eq('asdasd');
-
-			// $subquery->_whereStatement->valPrefix = 'sub';
-			// $subquery
-			// ->where('u.pass')->eq('asdasd')
-			// ->where('u.pass')->eq('asdasd');
-		// })
-		// ->where('u.pass')->ne('asdasd')
-		// ->where('u.pass')->lt(18)
-		// ->where('u.pass')->gt(19)
-		// ->where('u.pass')->ltoe(19)
-		// ->where('u.pass')->gtoe(19)
-		// ->where('u.pass')->between(19, 25)
-		// ->where('u.pass')->notBetween(19, 25)
-		// ->where('u.pass')->in(['asdasd', 'werwer'])
-		// ->where('u.pass')->notIn(['asdasd', 'werwer'])
-		// ->where('u.pass')->like('%asdasd%')
-		// ->where('u.pass')->notLike('%asdasd%')
-		/*->where('u.pass', '=', 'xcvxcvcvx')*/
-		/*->orWhere('u.pass', '=', 'xcvxcvcvx')*/
-		;
-
 		$s = $this->getSelect()
 		->select(['sa.*'])
+		// ->distinct()
 		->from(['system_admin' => 'sa'])
 		// ->where('sa.id')->in(function($sub){
 			
@@ -54,6 +19,8 @@ class User extends \Of\Database\Entity {
 		// 	->where('firstname')->eq('ron')
 		// 	->orWhere('lastname')->eq('perez');
 		// })
+		->min('id')
+		->max('id')
 		->group(function($sub){
 			$sub
 			->group(function($subd1){
@@ -71,11 +38,12 @@ class User extends \Of\Database\Entity {
 		;
 
 
-		// $data = $this->fetchAll($s);
+		$count = $this->count($s, 'id', 'sasd');
+		$data = $this->fetchAll($s);
 
 		echo "<pre>";
-		// print_r($data );
-		// echo $s->dumpQuery();
+		print_r($count);
+		echo $s->dumpQuery();
 
 		die;
 	}
